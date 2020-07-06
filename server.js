f6431578c9c0ff3375b2990abbfc600296477a17
Mason <Mudland2721@gamil.com>
2020-07-05 20:32:41 -0600

    started find by man function not working but layout is done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const console_table = require("console.table");
-const clear = require("console-clear");
+const revert = require("console-clear");
 const allQuery = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.department_name, concat(manager.first_name," ",manager.last_name) as manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee as manager on employee.manager_id = manager.id';
 //connection pretences 
 const connection = mysql.createConnection({
@@ -38,6 +38,7 @@ function startSearch() {
 
             case 'View all Employees by Manager':
                 //function for this ()
+                employeeByManager();
                 break;
 
             case 'View all Employees by department':
@@ -55,15 +56,18 @@ function startSearch() {
             case 'Add Employee department':
                 //function for this ()
                 break;
+
+            case 'Exit':
+                //function for this ()
+                exitConsole();
+                break;
         }       
     })
 } startSearch();
 
 //function for all emp
 viewAll = () => {
-    //show table in console then return to main menu
-    // console.log("viewAll occurred");
-    
+    //show table in console then return to main menu    
     connection.query(allQuery, function (err, res) {
       if (err) throw err;
       console.log('OH LORD------------------------------', res);
@@ -72,3 +76,37 @@ viewAll = () => {
       startSearch();
     });
   };
+
+//function for kill app
+  exitConsole = () => {
+    revert;
+  };
+
+//function for show all emp by managers 
+    async function employeeByManager() {
+      // clear console then reRender
+    revert;
+    let manager = await connection.query("SELECT * FROM employee");
+    let mangersSelection = manager.map(({id, first_name, last_name}) => ({
+
+        name: first_name.concat(" ", last_name), 
+        value: id,
+
+    }));
+
+    
+    //specific manager Id for follow up prompt
+    let { userManagerId } = inquirer.prompt([
+        {
+            type: "list",
+            message: "Which employee Manager would you like to view?",
+            name: 'userManagerId',
+            choices: mangersSelection
+        }
+    ]);
+
+    let employees = await connection.query(allQuery, + "WHERE role.id;", userManagerId);
+
+    console.table(employees);
+    startSearch();
+}
